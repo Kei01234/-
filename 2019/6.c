1c3cc28857d2a1b95eb18c9ab3da2e59d66c5cc7
Keisuke Ishigami <keisuke.130@icloud.com>
2021-01-21 17:57:05 +0900

    GetAverage関数が動作しない不具合を修正

--- 2019/6.c
+++ 2019/6.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 double GetAverage(double *array, int num); //プロトタイプ宣言
-int main(void)
-{
+
+int main(void) {
     double height[10]={0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
     double ave;
     //-----
@@ -10,4 +10,20 @@ int main(void)
     //-----
     printf("Average = %f\n", ave);
     return 0;
+}
+
+double GetAverage(double *array, int num) {
+    for (int i = 0; i < num/2; ++i) {
+        double tmp;
+        tmp=array[i];
+        array[i]=array[num-1-i];
+        array[num-1-i]=tmp;
+    }
+
+    double sum=0;
+    for (int i = 0; i < num; ++i) {
+        sum+=array[i];
+    }
+
+    return sum/num;
 }
